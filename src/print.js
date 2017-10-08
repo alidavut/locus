684e366e727ea0f199688c72227562464b0f102d
rgrannell1 <r.grannell2@gmail.com>
2017-10-08 14:58:54 +0100

    documented print module

--- src/print.js
+++ src/print.js
@@ -1,44 +1,83 @@
-var fs = require('fs');
-var util = require('util');
-var color = require('cli-color');
-var highlight = require('cardinal').highlight;
-var syntaxTheme = require('cardinal/themes/tomorrow-night.js');
-var _ = require('lodash');
 
-exports.file = function(path, line) {
-  var file = highlight(
-    fs.readFileSync(path, 'utf-8'),
-    { theme: syntaxTheme }
-  );
+const fs = require('fs')
+const util = require('util')
+const color = require('cli-color')
+const highlight = require('cardinal').highlight
+const syntaxTheme = require('cardinal/themes/tomorrow-night.js')
+const _ = require('lodash')
 
-  console.log();
-  console.log(color.bold('File : ') + path + ' @ line: ' + line);
-  console.log();
 
-  var startLine = line > 7 ? line - 7 : 0;
-  var lines = file.split('\n');
 
-  for(var i = startLine; i < line + 3 && i < lines.length; i++) {
-    var maxLineNumberLength = line.toString().length;
 
-    if (line === i + 1) {
-      var lineNumber = ' => ' + (i + 1);
-    } else {
-      var lineNumber = i + 1;
-    }
+const print = { }
 
-    lineNumber = _.padStart(lineNumber + ': ', maxLineNumberLength + 6);
 
-    console.log(color.xterm(111)(lineNumber) + lines[i]);
-  }
 
-  console.log();
+
+
+/*
+ * @param {string} path the path to load.
+ * @parm {number} line the current line number in the file.
+ *
+ */
+
+print.file = (path, line) => {
+
+	// -- syntax-highlight the loaded, current, JS file.
+	const file = highlight(fs.readFileSync(path, 'utf-8'), {
+		theme: syntaxTheme
+	})
+
+	// -- display details about the file.
+	console.log( )
+	console.log(color.bold('File : ') + path + ' @ line: ' + line)
+	console.log( )
+
+	var startLine = line > 7 ? line - 7 : 0
+	var lines = file.split('\n')
+
+	for (var ith = startLine; ith < line + 3 && ith < lines.length; ith++) {
+
+		var maxLineNumberLength = line.toString( ).length
+
+		if (line === ith + 1) {
+			var lineNumber = ' => ' + (ith + 1)
+		} else {
+			var lineNumber = ith + 1
+		}
+
+		lineNumber = _.padStart(lineNumber + ': ', maxLineNumberLength + 6)
+
+		console.log(color.xterm(111)(lineNumber) + lines[ith])
+	}
+
+	console.log()
 }
 
-exports.success = function(result) {
-  console.log(color.greenBright(util.inspect(result, false, 1, true)));
+/*
+ * @param {object} result The value returned from the REPL.
+ *
+ * print a stringified result returned from an executed command.
+ *
+ */
+
+print.success = result => {
+	console.log(color.greenBright(util.inspect(result, false, 1, true)))
 }
 
-exports.error = function(err) {
-  console.log(color.redBright(err));
+/*
+ * @param {string} err an error result to display.
+ *
+ * print a error that occurred while running a command.
+ *
+ */
+
+print.error = err => {
+	console.log(color.redBright(err))
 }
+
+
+
+
+
+module.exports = print
